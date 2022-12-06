3a4e13592e64d480a3d7124c17049939c02d8ec5
DESKTOP-EUO1M7V\SaeedAdmin <shahriarysaeedreza@gmail.com>
2022-12-06 18:27:46 +0800

    2. Angular Basics

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'client';
+  products: any[];
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?PageSize=50&PageIndex=1&BrandId=1&TypeId=1&Search=Angular').subscribe({
+    next: (response: IPagination) => {
+      this.products = response.data;
+      console.log(this.products);
+    },
+    error: (e) => console.error(e)
+  }
+    );    
+  }
 }
+
+
